fab417df3ae28e74ebeccbd283e563b2bede8dc2
Hari prasad R <hariprasad3614@gmail.com>
2024-03-24 22:09:28 +0530

    completed Mark screen

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -14,6 +14,7 @@ class _SettingsPageState extends State<SettingsPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Colors.blue,
         title: const Text('Settings'),
         centerTitle: true,
       ),
